4012c2dc17741a93d031a4639396596575036895
TechnoVisionDev <thomasanthonypeters@gmail.com>
2021-11-15 13:22:38 -0800

    Login page frontend

--- src/main/java/backend/MyServlet.java
+++ src/main/java/backend/MyServlet.java
@@ -19,13 +19,15 @@ public class MyServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String fna = request.getParameter("restaurant");
-		String lna = request.getParameter("location");
-		if (fna.contentEquals("") || lna.contentEquals("")) {
-			out.println("<h4>Name is missing!</h4>"); 
-		} else {
-			out.println("<h4>Submitted! Yay!</h4>"); 
-		}
+		//String fna = request.getParameter("restaurant");
+		//String lna = request.getParameter("location");
+		out.println("<h4>Submitted! Yay!</h4>");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("HEY");
 	}
 	
 	public void destroy() {
